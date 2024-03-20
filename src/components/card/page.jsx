d9b1d66598c0a3b899b81a56ccdbb5ace4b90b06
HARSHIT <apsbqt@gmail.com>
2024-03-20 19:56:29 +0530

    first 1

--- src/components/card/page.jsx
+++ src/components/card/page.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from "react";
+import HCard from "../hoverCard/page";
 
 const Card = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="bg-gray-100 p-6">
-      <div className="max-w-xl mx-auto bg-white rounded-lg overflow-hidden shadow-lg">
+    <div className="bg-gray-100 p-6 sm:flex-col flex">
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ maxHeight: "90px" }}
+        className="max-w-xl bg-white rounded-lg overflow-hidden shadow-lg"
+      >
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">Card Title</div>
-          <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="px-6 py-4 flex flex-row space-x-4">
-          <img src="https://source.unsplash.com/random/100x100" alt="Image 1" className="rounded-full" />
-          <img src="https://source.unsplash.com/random/101x101" alt="Image 2" className="rounded-full" />
-          <img src="https://source.unsplash.com/random/102x102" alt="Image 3" className="rounded-full" />
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
         </div>
       </div>
+      {isHovered && <HCard />}
     </div>
   );
 };
---
+++ src/components/hoverCard/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const HCard = () => {
+  return (
+    <div
+      className="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition duration-300"
+      style={{ zIndex: 10 }}
+    >
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Card Title</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <div className="text-md font-semibold text-gray-600 mb-2">
+          Progress Bars
+        </div>
+        <div className="w-full h-8 bg-gray-200 rounded-full mb-4">
+          <div className="h-full bg-green-500 rounded-full"></div>
+        </div>
+        <div className="w-full h-8 bg-gray-200 rounded-full mb-4">
+          <div className="h-full bg-yellow-500 rounded-full"></div>
+        </div>
+        <div className="w-full h-8 bg-gray-200 rounded-full">
+          <div className="h-full bg-red-500 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HCard;
