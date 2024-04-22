2f5f5aad89c162f261c7bb2225aa2382a843c416
HARSHIT <apsbqt@gmail.com>
2024-04-22 10:19:49 +0530

    all setup configured w controller and router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
+      console.log(`🛞  Server is running at port : ${process.env.PORT}`);
     });
     app.on("error", (err) => {
       console.log("ERROR:", err);
